4a58f3d248a24213a97dc8b6aa689dd891e14f83
Jim Vargas <jimball1@hotmail.com>
2019-04-16 13:11:23 -0700

    HW 1 set 2 all done, formatting done I think

--- MATLAB_Tutorial/class_tutorial1.m
+++ MATLAB_Tutorial/class_tutorial1.m
@@ -1,10 +1,7 @@
 %Jim Vargas
-%MTH 410 Convex Optimization
-%HW 1 Set 2
 format compact
-
 "Tutorial here";
-%{
+
 "Create a row vector x starting with a, stepping by b, stopping at c"
 x1=1:pi:12
 "Row vector n=4 points linearly spaced btw and including a and b"
@@ -29,9 +26,8 @@ A7=rand(size(A6))
 A8=diag(x1)
 "Create vector with random permutation of integers up to n=3"
 x3=randperm(3)
-%}
 
-%{ 
+%{
     Some vector/matrix operations
 Euclidian norm: norm(a)
     also length(a) for dim
@@ -50,36 +46,3 @@ multiply/divide componentwise matrices:
         just multiplies or divides each element by the corresponding one in
         the other
 %}
-
-
-%Exercises
-%"Problem 1:"
-A=[1,100,2,6;
-    0,-6,2012,7;
-    1999,0,3,8;
-    1,-2,6,4];
-B=[8,-100,7,-2;
-    7,-6,-9,5;
-    8,-2,5,1;
-    2,-2,4,2];
-
-A+B, A-B, A*B, B*A, A^3, (A*B)', 126*(A+B), A.*B, A./B, A.^2
-det(A)
-eig(A)
-A(2,:)+A(4,:);
-
-%"Problem 2"
-X1=rand(4);
-X2=10+(20-10)*rand(4);
-
-%"Problem 3"
-a=[0, pi, 1, 2*pi];
-sin(a), exp(a), a.^2
-norm(a)
-
-%"Problem 4"
-y= -1+(1-(-1))*rand(1,100);
-max(y); min(y);
-
-
-
